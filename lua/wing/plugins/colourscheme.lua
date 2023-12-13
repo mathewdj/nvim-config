b7291be1a004c1dce876c54fe248ee0b4737a84c
green-seagull <9652156+green-seagull@users.noreply.github.com>
2023-12-14 07:38:11 +1100

    Updgrade colour theme and which-key

--- lua/wing/plugins/colourscheme.lua
+++ lua/wing/plugins/colourscheme.lua
@@ -1,10 +1,9 @@
-return { 
-    "bluz71/vim-nightfly-colors", 
-    name = "nightfly", 
-    lazy = false, 
-    priority = 1000, 
-    config = function()
-        vim.cmd [[colorscheme nightfly]]
-    end
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+  config = function()
+    vim.cmd [[colorscheme tokyonight]] 
+  end
 }
-
