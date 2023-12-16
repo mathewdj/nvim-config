68d0b8a903162a9e7df1e06536b7b9a6cee167b8
green-seagull <9652156+green-seagull@users.noreply.github.com>
2023-12-16 19:37:09 +1100

    Move files

--- init.lua
+++ init.lua
@@ -1,35 +1,5 @@
 require("wing.core.keymap")
-
-local opt = vim.opt
-opt.relativenumber = true
-opt.number = true
-opt.tabstop = 4
-opt.shiftwidth = 4
-opt.expandtab = true
-opt.autoindent = true
-
-opt.wrap = true
-
--- Search
-opt.ignorecase = true
-opt.smartcase = true
-
-opt.cursorline = true
-
--- backspace
-opt.backspace = "indent,eol,start"
-
--- clipboard
-opt.clipboard:append("unnamedplus")
-
--- split windows
-opt.splitright = true
-opt.splitbelow = true
-
--- Allow - to be part of a word
-opt.iskeyword:append("-")
+require("wing.core.config")
 
 require("wing.lazy")
-
-require("language_server_config")
-require("nvim_tree_config")
+require("wing.language_server_config")
