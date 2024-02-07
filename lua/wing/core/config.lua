367a1c20f8651abc8f893b16d471542875a6010f
green-seagull <9652156+green-seagull@users.noreply.github.com>
2024-02-08 05:49:24 +1100

    Allow shared clipboard from vim

--- lua/wing/core/config.lua
+++ lua/wing/core/config.lua
@@ -5,7 +5,6 @@ opt.tabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
-
 opt.wrap = true
 
 -- Search
@@ -17,12 +16,11 @@ opt.cursorline = true
 -- backspace
 opt.backspace = "indent,eol,start"
 
--- clipboard
-opt.clipboard:append("unnamedplus")
-
--- split windows
-opt.splitright = true
-opt.splitbelow = true
+-- Share clipboard
+opt.clipboard:append("unnamed")
 
 -- Allow - to be part of a word
 opt.iskeyword:append("-")
+
+-- Allow mouse
+#opt.mouse="a"
